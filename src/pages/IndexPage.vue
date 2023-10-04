735c3a474b9483145355f6f61e23e78307d6f915
Adam Schaal <amschaal@gmail.com>
2023-10-04 14:03:15 -0700

    initial quasar project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Coreomics LIMS</h1>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
